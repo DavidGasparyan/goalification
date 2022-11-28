417a0f14f26faddbe4a20dcf7db8dd96d7f32006
David Gasparyan <davidgasparyang@gmail.com>
2022-11-28 00:24:39 -0800

    created angular + nest + keycloak monorepo

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import {map, take} from "rxjs";
 
 @Component({
   selector: 'goalification-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+
+  constructor(private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<{ message: string }>('http://localhost:8003/api')
+      .pipe(take(1), map(cur => cur.message))
+      .subscribe(res => this.title = res);
+  }
 }
--- apps/ui/src/app/app.module.ts
+++ apps/ui/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
