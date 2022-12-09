376710a32501b4dc55ff8c6b80d59db238d59884
DavidGasparyan <davidgasparyang@gmail.com>
2022-12-09 10:45:04 -0800

    created init script for letsencrypt

---
+++ install-letsencrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Phase 1
+docker-compose -f ./docker-compose-initiate.yaml up -d nginx
+docker-compose -f ./docker-compose-initiate.yaml up certbot
+docker-compose -f ./docker-compose-initiate.yaml down
+
+# some configurations for let's encrypt
+curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+
+# Phase 2
+crontab ./crontab
+docker-compose -f ./compose.prod.yml -d up
